84ca46d76e96eb12f74897648fe071e9899b640d
rechar <884520248@qq.com>
2021-02-25 13:38:36 +0800

    力扣 中等 面试

---
+++ 最长公共子序列.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+#include<string.h>
+using namespace std;
+
+int longestCommonSubsequence(string text1, string text2);
+
+int main()
+{
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    int res = longestCommonSubsequence(s1, s2);
+    cout << res << endl;
+
+    return 0;
+}
+
+int longestCommonSubsequence(string text1, string text2)
+{
+    int m = text1.size();
+    int n = text2.size();
+    int dp[m + 1][n + 1];
+
+    for(int i = 0; i <= m; i++)
+        dp[i][0] = 0;
+    
+    for(int i = 0; i <= n; i++)
+        dp[0][i] = 0;
+
+
+    for(int i = 1; i <= m; i++)
+    {
+        for(int j = 1; j <= n; j++)
+        {
+            if(text1[i - 1] == text2[j - 1])
+            {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            }
+            else
+            {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    return dp[m][n];
+
+}
